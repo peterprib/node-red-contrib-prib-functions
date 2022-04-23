4d451cf04bd0f84c2fbd97cdaac880e098bf8e18
Peter Prib <peter.prib@gmail.com>
2022-04-23 18:58:44 +1000

    add add row

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,9 +1,19 @@
 const logger = new (require("node-red-contrib-logger"))("Matrix");
 logger.sendInfo("Copyright 2022 Jaroslav Peter Prib");
 
-function Matrix(rows,columns) {
+function Matrix(rows,columns,fill) {
+	if(rows instanceof Array) {
+		this.rows=rows.length;
+		if(this.rows==0) throw Error("expected rows")
+		const row=rows[0];
+		if(!row instanceof Array) throw Error("expected row to be array of columns")
+		this.columns=row.length;
+		if(this.columns==0) throw Error("expected Columns")
+		this.createVector().fillArray(rows);
+		return this;
+	} 
 	if(rows instanceof Object) {
-		Object.assign(this,rows)
+			Object.assign(this,rows)
 	} else {
 		this.rows=rows;
 		this.columns=columns;
@@ -11,12 +21,101 @@ function Matrix(rows,columns) {
 	this.createVector();
 	return this;
 }
+Matrix.prototype.addRow=function(vector){
+	if(this.size==this.sizeMax){
+		this.vector.copyWithin(0,this.columns,this.sizeMax);
+		this.rows--;
+		this.size-=this.columns;
+	}
+	this.vector.set(vector, this.rows*this.columns);
+	this.rows++;
+	this.size+=this.columns;
+	return this;
+}
+Matrix.prototype.consoleLog=function(label){
+	console.log({label:label,rows:this.rows,columns:this.columns,size:this.size,sizeMax:this.sizeMax})
+	return this;
+}
 Matrix.prototype.createVector=function(){
 	if(this.size==null) {
-		if(this.rows==null) throw Error("rows not specified")
+		if(this.rows==null){
+			if(this.rowsMax==null)  throw Error("rows or rowsMax not specified")
+			this.rows=0;
+			this.sizeMax=this.rowsMax*this.columns;
+		}
 		if(this.columns==null) throw Error("columns not specified")
-		if(this.sizeMax==null) this.sizeMax=this.rows*this.columns
+		this.size=this.rows*this.columns
+		if(this.sizeMax==null) this.sizeMax=this.size
+	} else {
+		if(this.columns==null) throw Error("columns not specified")
+		if(this.rows==null){
+			this.rows=0;
+		}  	
 	}
-	this.vector=new Float32Array();
+	this.vector=new Float32Array(this.sizeMax);
+	return this;
 }
+Matrix.prototype.equalsNearly=function(matrix,precision=6){
+	if(matrix instanceof Array) {
+		if(this.rows!=matrix.length) throw Error("rows counts not equal")
+		if(this.columns!=matrix[0].length) throw Error("columns counts not equal")
+		this.forEachRow((row,index)=>{
+			row.forEach(v,column=>matrix.equalsNearlyVector(v,get(row,column)))
+		})
+		return this;
+	}
+	if(this.rows!=matrix.rows) throw Error("rows counts not equal")
+	if(this.columns!=matrix.columns) throw Error("columns counts not equal")
+return this;
+}
+Matrix.prototype.equalsNearlyValues=function(x,y,precision=6){
+		if(x==0){
+			if(y.toFixed(precision)==0) return this;
+		} else {
+			if((y/x).toFixed(precision)==1) return this;
+		}
+		throw Error("not equal");
+	return this;
+}
+Matrix.prototype.equalsNearlyVector=function(vector,precision=6){
+	this.vector.forEach((v,i,a)=>{
+		try{
+			this.equalsNearlyValues(v,vector[i],precision)
+		} catch(ex) {
+			throw Error('not equal, index '+i+' left: '+v+' right: '+vector[i]);
+		}
+	})
+	return this;
+}
+Matrix.prototype.fill=function(value, start, end){
+	this.vector.fill(value, start, end);
+	return this;
+}
+Matrix.prototype.fillArray=function(a){
+	const matrix=this;
+	a.forEach((columns)=>matrix.addRow(columns))
+	return this;
+}
+Matrix.prototype.forEachRow=call=>{
+	for(let index=0;index<this.rows;index++) {
+		call.apply(this,[this.getRow(index),index,this])
+	}
+	return this;
+}
+Matrix.prototype.get=function(row, column){
+	return this.vector[row*this.columns+column];
+}
+Matrix.prototype.getRow=row=>{
+	const start=rows*this.columns;
+	return this.vector.subarray(start,start+this.columns);
+}
+Matrix.prototype.getIndex=function(row, column){
+	const start=row*this.columns;
+	return row*this.columns+column;
+}
+Matrix.prototype.setRow=function(vector,row){
+	this.vector.set(vector, row*this.columns);
+	return this;
+}
+
 module.exports=Matrix;
--- test/matrix/01base.js
+++ test/matrix/01base.js
@@ -1,18 +1,91 @@
 const assert=require('assert');
 const Matrix=require("../../matrix/matrix");
 describe("matrix 01base", function() {
-	it('create', function(done) {
+	it('create no rows', function(done) {
 		assert.throws(()=>new Matrix(),Error)
 		assert.throws(()=>new Matrix(2),Error)
 		assert.throws(()=>new Matrix({rows:2}),Error("columns not specified"))
+		assert.throws(()=>new Matrix([]),Error)
+		assert.throws(()=>new Matrix([1,2,4]),Error)
+		assert.throws(()=>new Matrix([[]]),Error)
+		done();
+	});
+	it('create 2,3', function(done) {
 		const m=new Matrix(2,3); 
 		assert.strictEqual(m.rows,2);
 		assert.strictEqual(m.columns,3);
 		assert.strictEqual(m.size,6);
-		const m1=new Matrix({rows:2,columns:3}); 
-		assert.strictEqual(m1.rows,2);
-		assert.strictEqual(m1.columns,3);
-		assert.strictEqual(m1.size,6);
+		done();
+	});
+	it('create {rows:2,columns:3} ', function(done) {
+		const m=new Matrix({rows:2,columns:3}); 
+		assert.strictEqual(m.rows,2);
+		assert.strictEqual(m.columns,3);
+		assert.strictEqual(m.size,6);
+		done();
+	});
+	it('getIndex', function(done) {
+		const m=new Matrix({rows:2,columns:3}); 
+		assert.strictEqual(m.getIndex(0,0),0);
+		assert.strictEqual(m.getIndex(0,1),1);
+		assert.strictEqual(m.getIndex(1,1),4);
+		assert.strictEqual(m.getIndex(1,2),5);
+		done();
+	});
+	it('equalsNearlyValues', function(done) {
+		const m=new Matrix({rows:2,columns:3});
+		assert.throws(()=>m.equalsNearlyValues(1,0))
+		assert.throws(()=>m.equalsNearlyValues(0,1))
+		assert.doesNotThrow(()=>m.equalsNearlyValues(0,0));
+		assert.doesNotThrow(()=>m.equalsNearlyValues(1,1));
+		assert.throws(()=>m.equalsNearlyValues(1,1.00001));
+		assert.doesNotThrow(()=>m.equalsNearlyValues(1,1.00001,2));
+		assert.throws(()=>m.equalsNearlyValues(1.000001,1));
+		assert.doesNotThrow(()=>m.equalsNearlyValues(1.000001,1,2));
+		assert.doesNotThrow(()=>m.equalsNearlyVector([0,0,0,0,0,0]));
+		assert.throws(()=>m.equalsNearlyVector([1,2,3,4,5,6]));
+		done();
+	});
+	it('fill', function(done) {
+		const m=new Matrix({rows:2,columns:3});
+		m.fill(1,0,2) 
+		assert.doesNotThrow(()=>m.equalsNearlyVector([1,1,0,0,0,0]));
+		m.fill(3,3,6) 
+		assert.doesNotThrow(()=>m.equalsNearlyVector([1,1,0,3,3,3]));
+		done();
+	});
+	it('setRow', function(done) {
+		const m=new Matrix({rows:2,columns:3});
+		m.setRow([1,2,3],1) 
+		assert.doesNotThrow(()=>m.equalsNearlyVector([0,0,0,1,2,3]));
+		done();
+	});
+	it('addRow', function(done) {
+		const m=new Matrix({rows:2,columns:3}); //create a full zeroed
+		m.addRow([00,01,02]);
+		assert.doesNotThrow(()=>m.equalsNearlyVector(new Float32Array([0,0,0,0,1,2])));
+		m.addRow([10,11,12]);
+		assert.doesNotThrow(()=>m.equalsNearlyVector(new Float32Array([0,01,02,10,11,12])));
+		m.addRow([20,21,22]);
+		assert.doesNotThrow(()=>m.equalsNearlyVector(new Float32Array([10,11,12,20,21,22])));
+		done();
+	});
+	it('addRow start empty', function(done) {
+		const m=new Matrix({columns:3,rowsMax:2}); //create a full zeroed
+		m.addRow([00,01,02]);
+		assert.doesNotThrow(()=>m.equalsNearly([[0,1,2]]));
+		m.addRow([10,11,12]);
+		assert.doesNotThrow(()=>m.equalsNearly([[0,1,2],[10,11,12]]));
+		m.addRow([20,21,22]);
+		assert.doesNotThrow(()=>m.equalsNearly([[10,11,12],[20,21,22]]));
+		done();
+	});
+	it('create with array', function(done) {
+		const m=new Matrix([[00,01,02],[10,11,12]]); 
+		assert.strictEqual(m.rows,2);
+		assert.strictEqual(m.columns,3);
+		assert.strictEqual(m.size,6);
+		assert.doesNotThrow(()=>m.equalsNearlyVector(new Float32Array([00,01,02,10,11,12])));
 		done();
 	});
 });
